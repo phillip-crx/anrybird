c8e96ff26e7c2456aa5fae3bc1e0aa0738d4b462
phillip <siporadayalu2621@gmail.com>
2021-02-20 12:43:08 +0530

    commited

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,chain1
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,11 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    
+    log6 = new Log(230,180,80,PI/2)
+    chain1 = new chain(bird.body,log6.body)
+    
+
 
 }
 
@@ -58,6 +64,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    chain1.display();
 
     bird.display();
     platform.display();
